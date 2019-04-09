18abf12bab2a4b78aa4579e781aa5053d09cedce
Anthony Wan <anthonywan00@gmail.com>
2019-04-09 03:37:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,3 +8,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
   return playlist
 }
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
